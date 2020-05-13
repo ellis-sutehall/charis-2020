74d082612052ac585d1fce796784f1c044970a66
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-13 10:59:32 +0100

    Adds 404 page

--- 404.php
+++ 404.php
@@ -14,45 +14,17 @@ get_header();
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'charis-2020' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="container">
+					<header class="page-header">
+						<h1 class="title is-1 page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'charis-2020' ); ?></h1>
+					</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'charis-2020' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'charis-2020' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$charis_2020_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'charis-2020' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$charis_2020_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'charis-2020' ); ?></p>
+						<a href="/" class="button is-secondary">Go to homepage</a>
+						<a href="/shop" class="button is-secondary">Go to shop</a>
+					</div><!-- .page-content -->
+				</div>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
